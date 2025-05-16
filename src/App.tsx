3671161ce8689cb6d333a2a1912bdfc83ea515c7
ravshan77 <mygithub77@gmail.com>
2025-05-16 18:15:15 +0500

    add information

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Layout from '@/components/layout/Layout'
 import BlockedUsers from './pages/BlockedUsers'
 import WinnerUsers from './pages/WinnerUsers'
 import { PostUrlProvider } from './context/PostUrlContext'
+import Informations from './pages/Informations'
 
 function App() {
   return (
     <PostUrlProvider>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Informations />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/blocked-users/:shortcode" element={<BlockedUsers />} />
           <Route path="/winner-users/:shortcode" element={<WinnerUsers />} />
         </Routes>
---
+++ src/pages/Informations.tsx
@@ -0,0 +1,27 @@
+const Informations = () => {
+    
+  return (
+    <div className="flex flex-col justify-center items-center h-[80vh]">
+        <div>
+            <span className="font-bold">Release of Instagram.</span> 
+            <p className="p-1">  By participating, each entrant releases Instagram, its parent company, affiliates, 
+              subsidiaries, advertising and promotion agencies, partners, representatives, agents, 
+              successors and assigns (collectively, “Releasees”) from any and all liability in 
+              connection with this promotion and any prize won.
+            </p>
+        </div>
+
+
+        <div className="mt-8">
+          <span className="font-bold">Instagram’dan ozod qilish.</span>
+          <p className="p-1">  Ushbu tanlovda ishtirok etish orqali har bir qatnashchi Instagramni, uning bosh kompaniyasi, 
+            filiallari, sho‘’ba korxonalari, reklama va promosiya agentliklari, hamkorlari, vakillari, 
+            agentlari, voris va topshirilgan shaxslarini (“Ozod qilinganlar”) ushbu tanlov bilan bog‘liq har qanday talab, 
+            da’vo, shikoyat, zarar yoki majburiyatdan to‘liq ozod qiladi.
+          </p>    
+        </div> 
+    </div>
+  )
+}
+
+export default Informations
